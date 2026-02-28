4b0cf28af694b285f5634081096faf5cda041377
Mtawil <twlmed212@gmail.com>
2026-02-28 15:00:14 +0000

    adding rand method for the rectangle

--- geometrical_shapes/Rectangle.java
+++ geometrical_shapes/Rectangle.java
@@ -1,5 +1,7 @@
 package geometrical_shapes;
+
 import java.awt.Color;
+import java.util.Random;
 
 public class Rectangle implements Drawable {
     private final Point topLeft;
@@ -8,6 +10,16 @@ public class Rectangle implements Drawable {
     public Rectangle(Point topLeft, Point bottomRight) {
         this.topLeft = topLeft;
         this.bottomRight = bottomRight;
+    }
+
+    public static Rectangle random(int maxX, int maxY) {
+        Random rand = new Random();
+        int x1 = rand.nextInt(maxX);
+        int y1 = rand.nextInt(maxY);
+        int x2 = rand.nextInt(maxX);
+        int y2 = rand.nextInt(maxY);
+        return new Rectangle(new Point(Math.min(x1, x2), Math.min(y1, y2)),
+                             new Point(Math.max(x1, x2), Math.max(y1, y2)));
     }
 
     @Override
